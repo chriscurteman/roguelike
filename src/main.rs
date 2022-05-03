cd5fb50edfa5ecfb1480f7bd464261a5f50c8610
Chris <36980688+ccurteman@users.noreply.github.com>
2022-05-02 23:40:02 -0500

    Map and Tile System

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,56 @@
-// This file is generated automatically. Do not edit it directly.
-// See the Contributing section in README on how to make changes to it.
 use tcod::colors::*;
 use tcod::console::*;
 
-// actual size of the window
+// size of the window
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
-const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
+// size of the map
+const MAP_WIDTH: i32 = 80;
+const MAP_HEIGHT: i32 = 45;
+
+// tile colors
+const COLOR_DARk_WALL: Color = Color { 
+    r: 0, 
+    g: 0, 
+    b: 100,
+};
+const COLOR_DARK_GROUND: Color = Color {
+    r: 50,
+    g: 50,
+    b: 150,
+};
+
+// 20 frames-per-second maximum
+const LIMIT_FPS: i32 = 20; 
+
+type Map = Vec<Vec<Tile>>;
+
+struct Game {
+    map: Map,
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Tile {
+    blocked: bool,
+    block_sight: bool,
+}
+
+impl Tile {
+    pub fn empty() -> Self {
+        Tile { 
+            blocked: false, 
+            block_sight: false 
+        }
+    }
+
+    pub fn wall() -> Self {
+        Tile { 
+            blocked: true, 
+            block_sight: true 
+        }
+    }
+}
 
 struct Tcod {
     root: Root,
@@ -16,6 +59,7 @@ struct Tcod {
 
 // Generic Entity
 /// contains entity metadata
+#[derive(Debug)]
 struct Entity {
     x: i32,
     y: i32,
@@ -23,7 +67,7 @@ struct Entity {
     color: Color,
 }
 
-/// contains the code for entity functions
+// contains the code for entity functions
 impl Entity {
     pub fn new(
         x: i32, 
@@ -34,17 +78,20 @@ impl Entity {
         Entity { x, y, char, color }
     }
 
-    //// move by the given input amount
+    /// move by the given input amount
     pub fn move_by(
         &mut self, 
         dx: i32, 
-        dy: i32
+        dy: i32,
+        game: &Game
     ) {
-        self.x += dx;
-        self.y += dy;
+        if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
+            self.x += dx;
+            self.y += dy;
+        }
     }
 
-    //// set the color and draw the ascii character
+    /// set the color and draw the ascii character
     pub fn draw(
         &self, 
         con: &mut dyn Console
@@ -54,8 +101,63 @@ impl Entity {
     }
 }
 
+fn make_map() -> Map {
+    // fill the map with unblocked tiles
+    let mut map = vec![vec![
+        Tile::empty(); 
+        MAP_HEIGHT as usize]; 
+        MAP_WIDTH as usize];
 
-fn handle_keys(tcod: &mut Tcod, player: &mut Entity) -> bool {
+        map[30][22] = Tile::wall();
+        map[50][22] = Tile::wall();
+
+        map
+}
+
+fn render_all(tcod: &mut Tcod, game: &Game, entities: &[Entity])
+{
+    // draw all entities in the list
+    for entity in entities {
+        entity.draw(&mut tcod.con);
+    }
+
+    // go through all tiles and set their background color
+    for y in 0..MAP_HEIGHT {
+        for x in 0..MAP_WIDTH {
+            let wall = game.map[x as usize][y as usize].block_sight;
+            if wall {
+                tcod.con
+                    .set_char_background(
+                        x, 
+                        y, 
+                        COLOR_DARk_WALL, 
+                        BackgroundFlag::Set
+                    );
+            } else {
+                tcod.con
+                    .set_char_background(
+                        x, 
+                        y, 
+                        COLOR_DARK_GROUND, 
+                        BackgroundFlag::Set
+                    );
+            }
+        }
+    }
+
+    // blit the contents of "con" to the root console
+    blit(
+        &tcod.con,
+        (0, 0),
+        (MAP_WIDTH, MAP_HEIGHT),
+        &mut tcod.root,
+        (0, 0),
+        1.0,
+        1.0,
+    );
+}
+
+fn handle_keys(tcod: &mut Tcod, game: &Game, player: &mut Entity) -> bool {
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -73,10 +175,10 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Entity) -> bool {
         Key { code: Escape, .. } => return true, // exit game
 
         // movement keys
-        Key { code: Up, .. } => player.move_by(0, -1),
-        Key { code: Down, .. } => player.move_by(0, 1),
-        Key { code: Left, .. } => player.move_by(-1, 0),
-        Key { code: Right, .. } => player.move_by(1, 0),
+        Key { code: Up, .. } => player.move_by(0, -1, game),
+        Key { code: Down, .. } => player.move_by(0, 1, game),
+        Key { code: Left, .. } => player.move_by(-1, 0, game),
+        Key { code: Right, .. } => player.move_by(1, 0, game),
 
         _ => {}
     }
@@ -85,16 +187,30 @@ fn handle_keys(tcod: &mut Tcod, player: &mut Entity) -> bool {
 }
 
 fn main() {
-    tcod::system::set_fps(LIMIT_FPS);
+    tcod::system::set_fps(
+        LIMIT_FPS
+    );
 
     let root = Root::initializer()
-        .font("arial10x10.png", FontLayout::Tcod)
-        .font_type(FontType::Greyscale)
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("Rust/libtcod tutorial")
+        .font(
+            "arial10x10.png", 
+            FontLayout::Tcod)
+        .font_type(
+            FontType::Greyscale
+        )
+        .size(
+            SCREEN_WIDTH, 
+            SCREEN_HEIGHT
+        )
+        .title(
+            "Rust/libtcod tutorial"
+        )
         .init();
 
-    let con = Offscreen::new(SCREEN_WIDTH, SCREEN_HEIGHT);
+    let con = Offscreen::new(
+        MAP_WIDTH, 
+        MAP_HEIGHT
+    );
 
     let mut tcod = Tcod { 
         root, 
@@ -120,29 +236,38 @@ fn main() {
         npc
     ];
 
+    let game = Game {
+        map: make_map(),
+    };
+
+    // Main Game Loop
     while !tcod.root.window_closed() {
         // clears the previous frame
         tcod.con.clear();
 
         for entity in &entities {
-            entity.draw(&mut tcod.con);
+            entity.draw(
+                &mut tcod.con
+            );
         }
-                
-        blit(
-            &tcod.con,
-            (0,0), 
-            (SCREEN_WIDTH, SCREEN_HEIGHT), 
-            &mut tcod.root, 
-            (0,0), 
-            1.0, 
-            1.0
+
+        // render the game with entities
+        render_all(
+            &mut tcod, 
+            &game, 
+            &entities
         );
 
+        // draw everything on the console at the same time
         tcod.root.flush();
 
         // handle keys and exit game if needed
         let player = &mut entities[0];
-        let exit = handle_keys(&mut tcod, player);
+        let exit = handle_keys(
+            &mut tcod, 
+            &game,
+            player
+        );
         if exit {
             break;
         }
